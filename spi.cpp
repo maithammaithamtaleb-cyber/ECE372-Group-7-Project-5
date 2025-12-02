8710ef2a925f578c126188f8c03691631b89980b
maithammaithamtaleb-cyber <maithammaithamtaleb@gmail.com>
2025-12-02 11:11:33 -0700

    Update spi.cpp

--- spi.cpp
+++ spi.cpp
@@ -1,4 +1,3 @@
-
 #include "spi.h"
 #include <avr/io.h>
 #include <util/delay.h>
@@ -8,7 +7,6 @@
 // MISO = PB3
 // SCK  = PB1
 // SS   = PB0  
-
 
 // SPI Initialization (Mode 0, MSB first)
 void SPI_Init()
@@ -21,16 +19,6 @@ void SPI_Init()
 
     // Enable SPI in Master mode, fosc/16, Mode-0
     SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR0);
-}
-
-// Initialize MAX7221 for 8×8 LED matrix
-void MAX7219_Init()
-{
-    SPI_SendCommand(MAX_REG_SCANLIMIT, 0x07); // Show all 8 rows
-    SPI_SendCommand(MAX_REG_DECODE,    0x00); // Matrix mode
-    SPI_SendCommand(MAX_REG_SHUTDOWN,  0x01); // Normal operation
-    SPI_SendCommand(MAX_REG_TEST,      0x00); // No test mode
-    SPI_SendCommand(MAX_REG_INTENSITY, 0x08); // Brightness (0–15)
 }
 
 // Send a single byte over SPI
@@ -49,6 +37,15 @@ void SPI_SendCommand(int reg, int value)
     PORTB |= (1 << PB0);   // CS HIGH (latch)
 }
 
+// Initialize MAX7221 for 8×8 LED matrix
+void MAX7219_Init()
+{
+    SPI_SendCommand(MAX_REG_SCANLIMIT, 0x07); // Show all 8 rows
+    SPI_SendCommand(MAX_REG_DECODE,    0x00); // Matrix mode
+    SPI_SendCommand(MAX_REG_SHUTDOWN,  0x01); // Normal operation
+    SPI_SendCommand(MAX_REG_TEST,      0x00); // No test mode
+    SPI_SendCommand(MAX_REG_INTENSITY, 0x08); // Brightness (0–15)
+}
 
 // Display 8 bytes on matrix (1 byte = 1 row)
 void MAX7219_DisplayBuffer(const int buffer[8])
@@ -58,4 +55,3 @@ void MAX7219_DisplayBuffer(const int buffer[8])
         SPI_SendCommand(row + 1, buffer[row]);
     }
 }
-
